691fa509bc5209b988a4fbef09b0f70aa8297934
orhan9949 <47855426+orhan9949@users.noreply.github.com>
2021-06-30 18:09:13 +0300

    blank

---
+++ index.js
@@ -0,0 +1,17 @@
+var vm = new Vue ({
+    el: '#blank-blog',
+    data: {
+        name_company: "",
+        name: "",
+        surname: "",
+        patronymic: "",
+        position: "",
+        writen_date_dd: "",
+        writen_date_mm: "",
+        writen_date_yyyy: "",
+        writen_dismissal_dd: "",
+        writen_dismissal_mm: "",
+        writen_dismissal_yyyy: ""
+    }
+
+})
